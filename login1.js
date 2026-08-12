b213a4c8685e6b119e26ac3114e067c990ec8607
THARA HUOKAING <tharahuokaing@gmail.com>
2026-08-12 07:30:49 +0700

    Update financial figures in dashboard cards

--- login1.js
+++ login1.js
@@ -187,7 +187,7 @@
                 <div class="dashboard-card">
                     <a href="https://tharahuokaing.github.io/total_account/" style="text-decoration: none; color: inherit; display: block; height: 100%; width: 100%;">
                         <img src="total_account.jpg" alt="Accounts Icon" class="card-icon" style="width:150px; height:150px;">
-                        <h3 id="accountCount">$15,000</h3>
+                        <h3 id="accountCount">$40,000,000</h3>
                         <p>Total Accounts</p>
                         <span class="card-link">View Details</span>
                     </a>
@@ -197,7 +197,7 @@
                 <div class="dashboard-card">
                     <a href="https://tharahuokaing.github.io/deposit/" style="text-decoration: none; color: inherit; display: block; height: 100%; width: 100%;">
                         <img src="total_deposit.jpg" alt="Deposits Icon" class="card-icon" style="width:150px; height:150px;">
-                        <h3 id="depositTotal">$700,050,000</h3>
+                        <h3 id="depositTotal">$40,000,000</h3>
                         <p>Total Deposits</p>
                         <span class="card-link">View Details</span>
                     </a>
@@ -207,7 +207,7 @@
                 <div class="dashboard-card">
                     <a href="https://tharahuokaing.github.io/transaction/" style="text-decoration: none; color: inherit; display: block; height: 100%; width: 100%;">
                         <img src="transaction_volumn.jpg" alt="Transactions Icon" class="card-icon" style="width:150px; height:150px;">
-                        <h3 id="txCount">$0.00</h3>
+                        <h3 id="txCount">$1,500,000</h3>
                         <p>Transactions Processed</p>
                         <span class="card-link">View Log</span>
                     </a>

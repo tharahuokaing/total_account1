fc388117d657540ef8f5ff7d39e0148efb6315b7
THARA HUOKAING <tharahuokaing@gmail.com>
2026-08-21 16:56:18 +0700

    Update link for Total Accounts card

--- login1.js
+++ login1.js
@@ -203,7 +203,7 @@
 
                 <!-- Card 1: Total Accounts -->
                 <div class="dashboard-card">
-                    <a href="https://tharahuokaing.github.io/total_account/" style="text-decoration: none; color: inherit; display: block; height: 100%; width: 100%;">
+                    <a href="https://tharahuokaing.github.io/bank1/" style="text-decoration: none; color: inherit; display: block; height: 100%; width: 100%;">
                         <img src="total_account.jpg" alt="Accounts Icon" class="card-icon" style="width:150px; height:150px;">
                         <h3 id="accountCount">$55,550,000.00</h3>
                         <p>Total Accounts</p>
